e49619105c9d00794dc0a97d79c826720c80bb26
FlowQATeam <flowqateam@gmail.com>
2021-05-17 13:58:13 +0530

    First commit to FlowQATeam

---
+++ UserCodeModule1.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Created by Ranorex
+ * User: E1238621
+ * Date: 20-Apr-21
+ * Time: 3:54 PM
+ * 
+ * To change this template use Tools > Options > Coding > Edit standard headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Drawing;
+using System.Threading;
+using WinForms = System.Windows.Forms;
+
+using Ranorex;
+using Ranorex.Core;
+using Ranorex.Core.Testing;
+
+namespace SSPC_iOS
+{
+    /// <summary>
+    /// Description of UserCodeModule1.
+    /// </summary>
+    [TestModule("38B8B8D5-058E-4FAF-8592-D207855CD333", ModuleType.UserCode, 1)]
+    public class UserCodeModule1 : ITestModule
+    {
+        /// <summary>
+        /// Constructs a new instance.
+        /// </summary>
+        public UserCodeModule1()
+        {
+            // Do not delete - a parameterless constructor is required!
+        }
+
+        /// <summary>
+        /// Performs the playback of actions in this module.
+        /// </summary>
+        /// <remarks>You should not call this method directly, instead pass the module
+        /// instance to the <see cref="TestModuleRunner.Run(ITestModule)"/> method
+        /// that will in turn invoke this method.</remarks>
+        void ITestModule.Run()
+        {
+            Mouse.DefaultMoveTime = 300;
+            Keyboard.DefaultKeyPressTime = 100;
+            Delay.SpeedFactor = 1.0;
+        }
+    }
+}
